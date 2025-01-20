aabddfb6ccea8eaf315c3862e2c7826426d888e4
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-01-20 21:45:51 +0900

    コンフリクト修正

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
 module NonfryerRecipes
-=======
-module Myapp
->>>>>>> 6cc88941f58a74bae306fa66808c33cf8c24726c
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
