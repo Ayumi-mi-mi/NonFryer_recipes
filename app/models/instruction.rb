41f6c84b737e8138fc343701cef4a8ebf5f83e25
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-01-28 14:02:25 +0900

    作り方項目修正：複数項目が保存可能に

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,11 +1,11 @@
 class Instruction < ApplicationRecord
   belongs_to :recipe
-  before_validation :set_step_number, on: :create
+
+  after_create :set_step_number
 
   private
 
   def set_step_number
-    # 自動的にステップ番号を設定
-    self.step_number ||= recipe.instructions.count + 1
+    update_column(:step_number, recipe.instructions.count)
   end
 end
