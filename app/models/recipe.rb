c589a0a4f1431c2ac6d9821ae62bee669c330709
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-01-29 23:04:28 +0900

    画像投稿機能実装

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,8 @@ class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   has_many :instructions, dependent: :destroy
 
+  has_one_attached :main_image
+
   accepts_nested_attributes_for :heats, allow_destroy: true
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :instructions, allow_destroy: true, reject_if: :all_blank
@@ -11,4 +13,6 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :model, presence: true, length: { maximum: 255 }
   validates :point, length: { maximum: 65_535 }
+
+  validates :main_image, blob: { content_type: [ "image/png", "image/jpg", "image/jpeg" ], size_range: 1..(5.megabytes) }
 end
