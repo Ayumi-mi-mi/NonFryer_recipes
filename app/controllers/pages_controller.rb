1a59495cd9156d6dc12d303de9f9180a350cc92c
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-07 20:50:15 +0900

    トップページの検索欄にオートコンプリート機能実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   def home
-    keywords = params.dig(:q, :tags_name_or_title_cont_any).to_s.strip.split(/\p{blank}+/)
-    @q = Recipe.ransack(tags_name_or_title_cont_any: keywords.presence || [ "" ])
+    keywords = params.dig(:q, :tags_name_or_title_cont_any).to_s.split(/[[:space:]]/)
+    @q = Recipe.ransack(tags_name_or_title_cont_any: keywords)
     @recipes = @q.result(distinct: true).order("created_at desc")
+  end
+
+  def autocomplete
+    query = params[:q].to_s.split(/[[:space:]]/)
+    @recipes = Recipe.ransack(title_or_tags_name_cont_any: query).result(distinct: true).limit(10)
+
+    render json: @recipes.map { |recipe| { label: recipe.title, value: recipe.title } }
   end
 end
---
+++ app/javascript/controllers/autocomplete_controller.js
@@ -0,0 +1,7 @@
+import { Application } from '@hotwired/stimulus'
+import { Autocomplete } from 'stimulus-autocomplete'
+
+const application = Application.start()
+application.register('autocomplete', Autocomplete)
+
+
