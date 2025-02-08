bc1f48c1b3e1529462fc53a1deed8e03af2688e6
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-08 13:15:16 +0900

    CItest修正

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,7 +1,2 @@
-import { Application } from '@hotwired/stimulus'
-import { Autocomplete } from 'stimulus-autocomplete'
-
-const application = Application.start()
-application.register('autocomplete', Autocomplete)
 
 
