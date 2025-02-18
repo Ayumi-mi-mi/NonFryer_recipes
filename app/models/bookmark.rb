be735f2364e29352b52180c47b3b0ca661c685b4
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-18 18:25:36 +0900

    マイグレーションファイル・モデル記述

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :board
+
+  validates :user_id, uniqueness: { scope: :recipe_id }
 end
--- app/models/heat.rb
+++ app/models/heat.rb
@@ -1,6 +1,6 @@
 class Heat < ApplicationRecord
   belongs_to :recipe
 
-  validates :time, presence: true
-  validates :temperature, presence: true
+  validates :time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :temperature, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
