c05cac7676089d2a34eabe4250ea4f9aedf7ade5
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-27 22:47:10 +0900

    CIテスト修正

--- test/controllers/tops_controller_test.rb
+++ test/controllers/tops_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class TopsControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "should get index" do
     get root_url
     assert_response :success
   end
