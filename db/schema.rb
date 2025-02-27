f0f6866da48a74225e124f9b19350a0035680ab6
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-28 03:15:26 +0900

    不要なマイグレーションをdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_26_072617) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_25_035433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,11 +147,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_26_072617) do
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
-    t.string "unconfirmed_email"
-    t.string "activation_state", default: "active"
-    t.string "activation_token"
-    t.datetime "activation_token_expires_at"
-    t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
