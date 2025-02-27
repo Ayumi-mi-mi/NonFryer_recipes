9afa022f8232bd37f71e051dccde20840c7807c8
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-02-27 15:09:20 +0900

    CIチェック修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class UserMailerTest < ActionMailer::TestCase
   test "reset_password_email" do
     user = users(:example_user)
+    user.reset_password_token = "dummy_token"
+    user.save!
     mail = UserMailer.reset_password_email(user)
     assert_equal "Reset password email", mail.subject
     assert_equal [ "example@example.com" ], mail.to
