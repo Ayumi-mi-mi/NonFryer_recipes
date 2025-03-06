237421c7eb834cd090a41280f6f684a2d747b166
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-06 14:19:40 +0900

    lintチェック修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,23 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'NonFryer recipes',
-      title: 'ノンフライヤーのレシピ検索',
+      site: "NonFryer recipes",
+      title: "ノンフライヤーのレシピ検索",
       reverse: true,
-      charset: 'utf-8',
-      description: 'ノンフライヤーで作った料理だけを掲載するアプリです。これって何度で何分やるんだろう... もっと美味しく作るにはどうしたらいい？ どんなものが作れるの？ そんなお悩みを解決します。',
-      keywords: 'ノンフライヤー,レシピ,ノンフライヤーレシピ',
+      charset: "utf-8",
+      description: "ノンフライヤーで作った料理だけを掲載するアプリです。これって何度で何分やるんだろう... もっと美味しく作るにはどうしたらいい？ どんなものが作れるの？ そんなお悩みを解決します。",
+      keywords: "ノンフライヤー,レシピ,ノンフライヤーレシピ",
       canonical: request.original_url,
-      separator: '|',
+      separator: "|",
       og: {
         site_name: :site,
         title: :title,
         description: :description,
-        type: 'website',
+        type: "website",
         url: request.original_url,
-        image: image_url('NonFryer_recipes.png'),
-        local: 'ja-JP',
+        image: image_url("NonFryer_recipes.png"),
+        local: "ja-JP"
       }
     }
   end
 end
-
-
