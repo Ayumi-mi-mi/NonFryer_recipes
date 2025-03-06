28162e8cc5b41a6fd2fde853fa1d3fbc3fef7baa
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-06 14:17:02 +0900

    OGP画像設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'NonFryer recipes',
+      title: 'ノンフライヤーのレシピ検索',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ノンフライヤーで作った料理だけを掲載するアプリです。これって何度で何分やるんだろう... もっと美味しく作るにはどうしたらいい？ どんなものが作れるの？ そんなお悩みを解決します。',
+      keywords: 'ノンフライヤー,レシピ,ノンフライヤーレシピ',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('NonFryer_recipes.png'),
+        local: 'ja-JP',
+      }
+    }
+  end
 end
+
+
