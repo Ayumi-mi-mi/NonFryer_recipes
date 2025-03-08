b4eae6e3da624b3927597307fa2b467f2de62498
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-08 22:35:12 +0900

    CIチェック修正

--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
   test "should get oauth" do
-    get oauth_path(provider: "google")
+    get "/oauth/google"
+    puts response.body
+    puts response.status
     assert_response :success
   end
 
   test "should get callback" do
-    get oauth_callback_path(provider: "google")
+    get oauths_callback_url
     assert_response :success
   end
 end
