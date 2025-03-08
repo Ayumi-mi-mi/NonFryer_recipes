1dd1c1587e6c8418f3cf2234ae1d6020ad04248b
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-08 21:42:14 +0900

    googleログイン実装

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,28 @@
+class OauthsController < ApplicationController
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    if @user = login_from(provider)
+      redirect_to root_path, notice: "Googleアカウントでログインしました"
+    else
+      begin
+        @user = create_from(provider)
+
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice: "Googleアカウントでログインしました"
+      rescue
+        redirect_to root_path, alert: "Googleアカウントでのログインに失敗しました"
+      end
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+end
