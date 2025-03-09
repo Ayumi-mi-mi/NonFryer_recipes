d51e940b42954b9792fa724afef483cdb12e3201
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-09 13:16:39 +0900

    lintチェック修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :authentications, allow_destroy: true
 
   validates :password, presence: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { (new_record? || changes[:crypted_password])}
+  validates :password, confirmation: true, if: -> { (new_record? || changes[:crypted_password]) }
   validates :password_confirmation, presence: true, if: -> { (new_record? || changes[:crypted_password]) }
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
