95527cf8d2af85384448626a8e34177e13eeae8f
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-03-19 17:04:47 +0900

    CIテスト修正

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+  config.action_view.cache_template_loading = true
+  config.log_level = :debug
 end
