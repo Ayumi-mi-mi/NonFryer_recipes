c6597c60270ef3dd157d633ba991c33e9f9673b5
Ayumi Hironaka <mimic@ayuminoMacBook-Air.local>
2025-04-05 00:27:13 +0900

    lintチェック修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: Rails.application.credentials.dig(:mailer, :email)
-  default from: ENV.fetch('SMTP_FROM_ADDRESS', 'ayumari0806@example.com')
+  default from: ENV.fetch("SMTP_FROM_ADDRESS", "ayumari0806@example.com")
   layout "mailer"
 end
